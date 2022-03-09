d761c476dcbed4d12a3d8da9f550274a4857cb8a
ardelgad <adelgadoporras@gmail.com>
2022-03-09 13:21:24 +0100

    terminado

--- js/app.js
+++ js/app.js
@@ -1,2 +1,34 @@
 // Tu código aquí.
 
+//Selecciona los botones
+let prev = document.querySelector(".prevBtn")
+
+let next = document.querySelector(".nextBtn")
+
+//Crea una variable para guardar el valor de contador
+//Ponles un evento para que cuando hagas click el valor de la variable augmento o disminuya
+//Has que el valor de la variable y el valor cambie/reemplaze el del html
+
+
+
+let contador = 0;
+
+prev.addEventListener ("click", function() {
+    document.querySelector("#counter").innerHTML  = contador.toString();
+    contador--;
+    if(contador<-1){
+        document.querySelector("#counter").style.color = "red"
+    }
+    return contador;
+});
+
+next.addEventListener ("click", function() {
+    document.querySelector("#counter").innerHTML  = contador.toString();
+    contador++;
+    if(contador>1){
+        document.querySelector("#counter").style.color = "green"
+    }
+    return contador;
+});
+
+
